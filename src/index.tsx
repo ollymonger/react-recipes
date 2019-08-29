9395874224cf09172291738d557482774f19213e
Olly Monger <olly@yllo.co.uk>
2019-08-29 11:55:04 +0100

    Code review feedback

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,11 @@ const repositories = initialiseRepositories();
 
 const App: React.FunctionComponent = () => {
     return (
-        <Router>
-            <RepositoriesContext.Provider value={repositories}>
+        <RepositoriesContext.Provider value={repositories}>
+            <Router>
                 <Route path="/" exact component={AllRecipes} />
-            </RepositoriesContext.Provider>
-        </Router>
+            </Router>
+        </RepositoriesContext.Provider>
     );
 };
 
